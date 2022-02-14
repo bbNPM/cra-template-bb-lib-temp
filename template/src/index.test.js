df99b1de69ce2bff41c6edf92bd9e4b5f60593d6
BananaBb <samson8682@gmail.com>
2022-02-14 17:52:17 +0800

    UPT: lib temp with example

--- template/src/index.test.js
+++ template/src/index.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Button from './components/Button';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Button label="My Button" />);
+  const linkElement = screen.getByText(/My Button/i);
   expect(linkElement).toBeInTheDocument();
 });
